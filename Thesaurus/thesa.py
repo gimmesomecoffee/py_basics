ce9e1ca3006696a20873c1b733e2ca12052eebe1
gimmesomecoffee <ogrod0707@gmail.com>
2023-09-21 11:53:36 +0200

    added difflib

--- Thesaurus/thesa.py
+++ Thesaurus/thesa.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -6,15 +7,28 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did u mean %s instead? Enter Y if yes, or N if no. " % get_close_matches(w, data.keys())[0])
+        yn = yn.upper()
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exists, please double check"
+        else:
+            return "We didn't understand your entry"
     else:
         return "didnt find"
     
 
-while True:
-    word = input("Enter a word: ")
-    
-    print(translate(word))
-    
+
+word = input("Enter word: ")
+output = translate(word)
+
+if type(output) == list:
+        for item in output:
+             print(item)
+else:
+     print(output)
 
 
 
