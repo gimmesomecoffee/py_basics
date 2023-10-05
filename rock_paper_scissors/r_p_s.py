27026a5eea74bf031ceaba5dc2559b469bf913e6
gimmesomecoffee <ogrod0707@gmail.com>
2023-10-05 19:46:32 +0200

    modified, added 2 functions

--- rock_paper_scissors/r_p_s.py
+++ rock_paper_scissors/r_p_s.py
@@ -1,35 +1,59 @@
 import random
+from sys import exit
 
-user_wins = 0
-computer_wins = 0
 
-options =  ["rock","paper","scissors"]
-while True:
-    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
-    if user_input == "q":
-        break
+def determine_winner():
+    options =  ["rock","paper","scissors"]
+    user_wins = 0
+    computer_wins = 0
+    while True:
+        user_pick = input("Choose: Rock, Paper or Scissors? \n").lower()
+        random_number = random.randint(0,2)
+        # rock = 0, paper = 1, scissors = 2
+        computer_pick = options[random_number]
+        print(f"Computer picked {computer_pick}")
+        if user_pick == "q":
+            break
+        elif user_pick == computer_pick:
+            print("Draw!")
+            continue
+        elif user_pick == "rock" and computer_pick == "scissors":
+            print("You won!")
+            user_wins += 1
+            continue
+        elif user_pick == "paper" and computer_pick == "rock":
+            print("You won!")
+            user_wins += 1
+            continue
+        elif user_pick == "scissors" and computer_pick == "paper":
+            print("You won!")
+            user_wins += 1
+            continue
+        else:
+            print("You lost!")
+            computer_wins += 1
+            continue
+       
+    print(f"Scoreboard:\nUser: {user_wins}\nComputer: {computer_wins}")
+   
+
     
-    if user_input not in options:
-        continue
 
-    random_number = random.randint(0,2)
-    # rock = 0, paper = 1, scissors = 2
-    computer_pick = options[random_number]
-    print(f"Computer picked {computer_pick}")
-
-    if user_input == "rock" and computer_pick == "scissors":
-        print("You won!")
-        user_wins += 1
-        continue
-    elif user_input == "paper" and computer_pick == "rock":
-        print("You won!")
-        user_wins += 1
-        continue
-    elif user_input == "scissors" and computer_pick == "paper":
-        print("You won!")
-        user_wins += 1
+def playGame():
+    print("test")
+    decision = input("Do you want to play the game?\nType: Y or N\n")
+    if decision == "y":
+        determine_winner()
+    elif decision == "n":
+        print("See you later!")
+        exit()
     else:
-        print("You lost!")
-        computer_wins += 1
+        print("Try again")
+
+
     
-print(f"You won {user_wins}\nPC won {computer_wins} times\nBye!")
+      
+
+playGame()
+
+
